3265e22189155768b22cab02f0f30e0fad3d781f
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-07-12 23:06:32 -0300

    Linear Search: Solved

--- LinearSearch.rb
+++ LinearSearch.rb
@@ -1,21 +1,18 @@
 puts "Linear Search"
 puts "-------------"
 
-def search(array, number)
-  array.each_with_index do |element, index|
-    if element == number then
-      puts "Search sucessful"
-      puts "Element #{element} in position #{index} of array"
+def findIndexAndNumber(number, array)
+  i = 0 # Inicializador do array
+  size = array.length
+  while i < size do
+    if array[i] == number then
+      puts "Number #{number} founded at #{i} of array"
+      break
+    elsif i >= size
+    puts "Number not founded in array"
     end
+    i = i + 1
+  end
 end
 
-array = [10,50,30,70,80,60,20,90,40]
-
-key = 90
-
-array.each_with_index do |element, index|
-  if element == key then
-    puts "Search successful"
-    puts "Element #{element} in position #{index}"
-    break
-end
+findIndexAndNumber(56, [22,32,12,44,56])
