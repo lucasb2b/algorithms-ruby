0ed2f41f5d5ba1789005bf7ac35b9d8a537d900c
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-07-14 23:25:52 -0300

    Código corrigido

--- BinarySearch.rb
+++ BinarySearch.rb
@@ -1,42 +1,35 @@
 array = [2,5,8,12,16,23,38,56,72,91] # Array de números
-key = 8 # Elemento a ser encontrado
+key = 16 # Elemento a ser encontrado
 
 # Primeiro passo
 # Retornar a metade de um array
 
-def splitArray(array, key)
+def split_array(array, key, start_index, end_index)
   # Receber um array, Dividi-lo e retornar a parte conveniente
-  sizeArray = array.length # Pegar o comprimento total
-  halfIndex = (sizeArray / 2) - 1 # Temos aqui o índice do elemento do meio
-  if array[halfIndex] == key then
-    puts "Achou no meio do array"
-    puts "Achou o elemento #{array[halfIndex]}"
-  elsif key < array[halfIndex] then
-    i = 0
-    newArray = []
-    while i <= halfIndex do
-      newArray << array[i]
-      i += 1
-    end
-    puts "Lado Esquerdo"
-    puts newArray
-    splitArray(newArray,key)
+
+  return -1 if start_index > end_index
+  #sizeArray = array.length # Pegar o comprimento total
+  #halfIndex = (sizeArray / 2) - 1 # Temos aqui o índice do elemento do meio
+  half_index = (start_index + end_index) / 2
+  if array[half_index] == key
+    return half_index
+  elsif key < array[half_index]
+    split_array(array, key, start_index, half_index - 1)
   else
-    i = array.length
-    newArray = []
-    while i >= halfIndex do
-      newArray << array[i]
-      i = i - 1
-    end
-    puts "Lado Direito"
-    reverseArray = newArray.reverse
-    puts reverseArray
-    splitArray(reverseArray,key)
+    split_array(array, key, half_index + 1, end_index)
   end
 end
 
 def binarySearch(array, key) 
-  splitArray(array,key)
+  split_array(array,key,0,array.length - 1)
 end
 
-binarySearch(array,key)
+index = binarySearch(array,key)
+
+puts index
+
+if index == -1
+  puts "Não encontrado"
+else
+  puts "Elemento encontrado no índice #{index}"
+end
