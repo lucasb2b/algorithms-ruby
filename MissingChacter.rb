044ee40ec13bddd6a70bb2b864b91bdee8b950ec
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-07-16 16:22:32 -0300

    Missing Character: Parte II

--- MissingChacter.rb
+++ MissingChacter.rb
@@ -1,5 +1,6 @@
 def missing_character(phrase = 0)
-  alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  #alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  alphabet = ('a'..'z').to_a
   # Colocar a string em minúsculo
   downcase_string = phrase.downcase
   # Remover espaços em branco, pontos, vírgulas
@@ -7,6 +8,22 @@ def missing_character(phrase = 0)
   # Separar letra a letra colocando em um array
   array_letter = phrase_sanitize.chars
 
-  return array_letter.inspect
+  i = 0
+  m = 0
 
+  while i < array_letter.length
+    j = 0
+    while j < alphabet.length
+      if array_letter[i] == alphabet[j]
+        m = m + 1
+      end
+      j = j + 1
+    end
+    i += 1
+  end
+
+  return alphabet.length - m
 end
+
+ok = missing_character("The quick brown fox jumps over the lazy dog.")
+puts ok
