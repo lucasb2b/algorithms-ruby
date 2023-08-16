8c05b7f31a449b600f660a95bd02004a61f72b7d
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-08-15 23:01:01 -0300

    refactor: Conversion of 'MissingCharacter' from procedural to class and method oriented.

--- MissingChacter.rb
+++ MissingChacter.rb
@@ -1,30 +1,31 @@
 require 'set'
 
-def missing_character(phrase = 0)
-  alphabet = ('a'..'z').to_a
-  downcase_string = phrase.downcase
-  phrase_sanitize = downcase_string.gsub(/[\s,\.]/, '')
-  array_letter = phrase_sanitize.chars
-
-  # Criando um conjunto com todas as letras do alfabeto
-  alphabet_set = Set.new(alphabet)
-
-  # Removendo as letras da frase do conjunto
-  array_letter.each do |letter|
-    alphabet_set.delete(letter)
+class MissingCharacterFinder
+  def initialize(phrase)
+    @alphabet = ('a'..'z').to_a
+    @phrase_sanitize = phrase.downcase.gsub(/[\s,\.]/, '')
+    @array_letter = @phrase_sanitize.chars
   end
 
-  # Convertendo o conjunto de letras ausentes para um array
-  letters_missing = alphabet_set.to_a
+  def find_missing_letters
+    alphabet_set = Set.new(@alphabet)
 
-  # Exibindo as letras ausentes em um puts
-  puts "Letras ausentes na frase:"
-  puts letters_missing.join(', ')
+    @array_letter.each do |letter|
+      alphabet_set.delete(letter)
+    end
 
-  # Retorna a quantidade de letras ausentes
-  return letters_missing.size
+    alphabet_set.to_a
+  end
 
+  def display_missing_letters
+    letters_missing = find_missing_letters
+    puts "Letras ausentes na frase:"
+    puts letters_missing.join(', ')
+    letters_missing.size
+  end
 end
 
-ok = missing_character("Eu sou o melhor dos mundos")
-puts "Quantidade de letras ausentes: #{ok}"
+phrase = "Eu sou o melhor dos mundos"
+missing_character_finder = MissingCharacterFinder.new(phrase)
+missing_letters_count = missing_character_finder.display_missing_letters
+puts "Quantidade de letras ausentes: #{missing_letters_count}"
